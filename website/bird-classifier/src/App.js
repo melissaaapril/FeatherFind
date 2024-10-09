57b6bb695e61b0aa71e3d82dd74e4429ce404e1f
Melissa Castro <castromelissa2004@gmail.com>
2024-10-09 10:54:09 -0700

    fixed components

--- website/bird-classifier/src/App.js
+++ website/bird-classifier/src/App.js
@@ -1,45 +1,56 @@
 import React, { useState } from 'react';
-import './App.css';
 import { useDropzone } from 'react-dropzone';
+import './App.css';
 import Navbar from './Components/Navbar.jsx';
 import './Components/Navbar.css';
-
+import Footer from './Components/Footer.jsx';
+import './Components/Footer.css';
 
 //lets ste up the main drag and drop function
 function DragDropImageUploader () {
     const [uploadedFile, setUploadedFile] = useState(null);
 
-    //we will callback when a file is dropped*/}
-    // so it is triggered when file dropped*/}
+    //we will callback when a file is dropped
     const onDrop = (acceptedFiles) => { 
-        // error handling to only take the first file*/}
+        //error handling to only take the first file
         const file = acceptedFiles[0];
-        //read in file*/}
+        //read in file/}
         const reader = new FileReader();
         reader.onloadend = () => {
-            setUploadedFile(reader.result); //save the image preview*/}
+            setUploadedFile(reader.result);
         };
         reader.readAsDataURL(file); // convert image to base64
     };
-    //*using drop zone from react*/}
-    const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    //using drop zone from react/}
+    const { getRootProps, getInputProps} = useDropzone({
         onDrop,
         //only accept images
-        accept: 'image/*',
+        accept: 'image/',
         multiple: false,
 });
+
 return(
   <> 
+    {/*using react component for Navbar*/}
     <Navbar/>
     {/*creates the card object*/}
     <div className = 'card'>
       <div className = 'top'>
-        {/*text that is inside on the top of the box */}
-        <p>Drag or Drop Files</p>
+        {/*text that is inside on the top of the box*/}
+        <h1>Get Started</h1>
+        <p>1.Upload an image of a bird you wish to identify</p>
+        <p>2.Wait for the AI to identify the bird for you</p>
+        <img 
+          src = "/logo_transparent.png" 
+          alt = "company logo" 
+          style={{ width: '300px' }}
+        />
+
         </div>
+        {/* drag and drop area */}
         <div {...getRootProps()} className="drag-area">
           <input {...getInputProps()} />
-            {/*text that is inside the actual box */}
+            {/*text that is inside the actual box*/}
             <p>drag & drop image OR click to select files</p>
         </div>
         {uploadedFile && (
@@ -47,12 +58,9 @@ return(
             <img src={uploadedFile} alt="Uploaded" style={{ width: '200px' }} />
           </div>
         )}
-        <div>
-        <button type = 'button'>Start</button>
-        </div>
-        <div>
-        <button type = 'button'>Delete</button>
-        </div>
+      </div>
+      <div>
+      <Footer/>
       </div>
      </>
   );
--- website/bird-classifier/src/Components/Navbar.jsx
+++ website/bird-classifier/src/Components/Navbar.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import './Navbar.css';
+<link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=Merriweather&display=swap" rel="stylesheet"></link>
 
 function Navbar(){
     return(
         <nav>
-        <img src = "#" alt = "company logo"/>
-        <p>By WingWise</p>
-        <ul class = "nav_links">
-            <li><a href = "#">Home</a> </li>
-            <li><a href = "#">Browse</a> </li>
-            <li><a href = "#">Login</a> </li>
-            <li><a href = "#">Register Us</a> </li>
-            <li><a href = "#">About Us</a> </li>
+        <img src = "/logo_transparent.png" alt = "company logo"/>
+        <p>FeatherFind By WingWise</p>
+        <ul className = "nav_links">
+            <li><a href = "/">Home</a> </li>
+            <li><a href = "/">Browse</a> </li>
+            <li><a href = "/">Login</a> </li>
+            <li><a href = "/">Register Us</a> </li>
+            <li><a href = "Website/bird-classifier/src/AboutUs.js">About Us</a> </li>
         </ul>
     </nav>
     );
